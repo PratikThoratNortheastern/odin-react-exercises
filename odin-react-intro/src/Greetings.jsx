78c6e1c14f38a36d2ba3d47f5bbf144726ef46b8
Pratik Thorat <thorat.p@northeastern.edu>
2026-08-12 13:18:31 -0400

    Named imports assignment

---
+++ odin-react-intro/src/Greetings.jsx
@@ -0,0 +1,9 @@
+function Greetings() {
+  return <h1>"I swear by the floral bonnet, I will end you."</h1>;
+}
+
+function FavoriteFood() {
+  return <h1>Dal Rice</h1>;
+}
+
+export { Greetings, FavoriteFood };
